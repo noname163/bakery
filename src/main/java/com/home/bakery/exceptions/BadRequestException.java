b928139a4bb64ddd2ae0f2296a21c8071101b191
DangHuuDat <danghuudat163@gmail.com>
2023-04-11 16:56:46 +0700

    Add base project

---
+++ src/main/java/com/home/bakery/exceptions/BadRequestException.java
@@ -0,0 +1,13 @@
+package com.home.bakery.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException() {}
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/home/bakery/utils/PageableUtil.java
@@ -0,0 +1,28 @@
+package com.home.bakery.utils;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import com.home.bakery.exceptions.BadRequestException;
+
+@Component
+public class PageableUtil {
+    public Pageable getPageable(int offset, int limit, String fieldName, String sortType) throws BadRequestException {
+        if(sortType == null || sortType.isEmpty()){
+            sortType = "ASC";
+        }
+        if(fieldName== null|| fieldName.isEmpty()){
+            fieldName = "";
+        }
+        if (sortType.trim().equalsIgnoreCase("ASC")) {
+            return PageRequest.of(offset, limit, Sort.by(fieldName).ascending());
+        }
+        if (sortType.trim().equalsIgnoreCase("DESC")) {
+            return PageRequest.of(offset, limit, Sort.by(fieldName).descending());
+        }
+        throw new BadRequestException("Invalid sort type");
+    }
+}
+
