93b6d122ab0f433e79c1c7c3fd24f7f4c595e2c6
DangHuuDat <danghuudat163@gmail.com>
2023-04-14 17:04:20 +0700

    Add Component annotaion in Message class

--- src/main/java/com/home/bakery/exceptions/message/Message.java
+++ src/main/java/com/home/bakery/exceptions/message/Message.java
@@ -1,5 +1,8 @@
 package com.home.bakery.exceptions.message;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Message {
     public String objectExistMessage(String objectName, String value){
         return objectName+" name " + value + " already exist.";
